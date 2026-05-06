4ddf7f98aa14f96bd7ff91d3df1adabcb6e5039d
Nigromante <nigromante@github.com>
2026-05-06 01:07:58 -0400

    conform style

--- lua/loader/init.lua
+++ lua/loader/init.lua
@@ -437,7 +437,8 @@ require('lazy').setup(
                     -- Disable "format_on_save lsp_fallback" for languages that don't
                     -- have a well standardized coding style. You can add additional
                     -- languages here or re-enable it for the disabled ones.
-                    local disable_filetypes = { c = true, cpp = true }
+                    -- local disable_filetypes = { c = true, cpp = true }
+                    local disable_filetypes = { hpp = true }
                     if disable_filetypes[vim.bo[bufnr].filetype] then
                         return nil
                     else
@@ -449,11 +450,18 @@ require('lazy').setup(
                 end,
                 formatters_by_ft = {
                     lua = { 'stylua' },
+                    c = { 'clang-format' },
+                    cpp = { 'clang-format' },
                     -- Conform can also run multiple formatters sequentially
                     -- python = { "isort", "black" },
                     --
                     -- You can use 'stop_after_first' to run the first available formatter from the list
                     -- javascript = { "prettierd", "prettier", stop_after_first = true },
+                },
+                formatters = {
+                    ['clang-format'] = {
+                        prepend_args = { '--style=file' },
+                    },
                 },
             },
         },
