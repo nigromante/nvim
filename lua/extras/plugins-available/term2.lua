1fa536de3ddacb5685d1efa0a2a64019f95892e7
Nigromante <nigromante@github.com>
2026-05-05 23:28:55 -0400

    reformat code

--- lua/extras/plugins-available/term2.lua
+++ lua/extras/plugins-available/term2.lua
@@ -1,12 +1,12 @@
 local cmd_name = 'FloattyToggle'
 
 return {
-  -- 'nigromante/floatty.nvim',
-  dir = '~/.config/nvim/lua/plugins-dev/floatty.nvim',
-  opts = {
-    key_sequence = ':' .. cmd_name .. '<Cr>i',
-    command = cmd_name,
-    keymap = '<leader>tx',
-    description = '[T]oggle [X]terminal',
-  },
+    -- 'nigromante/floatty.nvim',
+    dir = '~/.config/nvim/lua/plugins-dev/floatty.nvim',
+    opts = {
+        key_sequence = ':' .. cmd_name .. '<Cr>i',
+        command = cmd_name,
+        keymap = '<leader>tx',
+        description = '[T]oggle [X]terminal',
+    },
 }
