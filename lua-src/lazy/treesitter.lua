469f522bfb20681eecb65370bdf679246ff30dbd
Nigromante <nigromante@github.com>
2026-05-08 00:32:08 -0400

    fix treesitter config

--- lua-src/lazy/treesitter.lua
+++ lua-src/lazy/treesitter.lua
@@ -1,7 +1,7 @@
 return {
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
-    main = 'nvim-treesitter.configs',
+    main = 'nvim-treesitter.config',
     opts = {
         ensure_installed = { 'bash', 'c', 'cpp', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
         -- Autoinstall languages that are not installed
